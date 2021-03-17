95038607749091074c75971155d0d96640c5ecf4
Fabian Peña <fabiancpl91@gmail.com>
2021-03-17 11:26:23 -0500

    Improving tutorial explanations

--- src/co/edu/unbosque/onewaysocket/DateClient.java
+++ src/co/edu/unbosque/onewaysocket/DateClient.java
@@ -12,12 +12,16 @@ public class DateClient {
 
     public static void main(String[] args) throws IOException {
 
+        // the server IP is required to stablish the connection
         if (args.length != 1) {
             System.err.println("Pass the server IP as the sole command line argument");
             return;
         }
 
+        // a Socket is instantiated and a request for pairing with the server is sent
+        // listening port on the server must be the same
         var socket = new Socket(args[0], 59090);
+        // getting and encoding from bytes the input stream
         var in = new Scanner(socket.getInputStream());
         System.out.println("Server response: " + in.nextLine());
 
--- src/co/edu/unbosque/onewaysocket/DateServer.java
+++ src/co/edu/unbosque/onewaysocket/DateServer.java
@@ -3,10 +3,11 @@ package co.edu.unbosque.onewaysocket;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.Date;
 
 /**
+ * Example from: https://cs.lmu.edu/~ray/notes/javanetexamples/
+ *
  * A simple TCP server. When a client connects, it sends the client the current
  * datetime, then closes the connection. This is arguably the simplest server
  * you can write. Beware though that a client has to be completely served its
@@ -16,12 +17,22 @@ public class DateServer {
 
     public static void main(String[] args) throws IOException {
 
-        try (var listener = new ServerSocket(59090)) {
+        // try-with-resources statement
+        // in this way, the socket is automatically closed at the end of the block
+        try (
+                // local variable type inference
+                // a ServerSocket is created for listening on the specified port
+                var listener = new ServerSocket(59090)
+        ) {
 
             System.out.println("The date server is running...");
 
             while (true) {
+                // when server accepts a client, the communication for other clients remains blocked
+                // a Socket needs to be instantiated for handling in and out messages
                 try (var socket = listener.accept()) {
+                    System.out.println("A new connection was stablished!");
+                    // decoding to bytes and sending the message to the client
                     var out = new PrintWriter(socket.getOutputStream(), true);
                     out.println(new Date().toString());
                 }
